5831ee0388050cac6f6b3004f15cfcb52239ad76
Alan <msalanfortuny@gmail.com>
2023-11-08 20:23:41 +0100

    exercises course basics of var dclaration

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-console.log("Hello, World")
 
+var indicator;
+var indicator = "poverty rate" // variable declaration
+var value = "15%" // variable declaration
+console.log(value,indicator)
+value = "5%" // change of variable content
+console.log(value,indicator)
+
+var petDog = "Rex"
+var petCat = "Pepper"
+console.log(petDog)
+//console.log(petCat)
+//console.log("My pet dog's name is: ",petDog)
+//console.log("My pet cat's name is: ",petCat)
+//var catSound="purr"
+//var dogSound="woof"
+//console.log(petDog,"says",dogSound)
+//console.log(petCat,"says",catSound)
+//catSound="meow"
+//console.log(petCat,"now says",catSound)
+
+//var petDog = 'Rex'; // Task 1 solution
+//console.log(petDog);
